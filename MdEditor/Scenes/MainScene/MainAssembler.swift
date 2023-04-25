95c6d18bd3ae29047c4378ecde375bd48a1f8ccb
Supodoco <76879150+Supodoco@users.noreply.github.com>
2023-04-25 18:47:04 +0300

    Update MainAssembler.swift

--- MdEditor/Scenes/MainScene/MainAssembler.swift
+++ MdEditor/Scenes/MainScene/MainAssembler.swift
@@ -16,9 +16,10 @@ class MainAssembler {
 	/// - Returns: Возвращает MainViewController для отображения главного экрана
 	static func assembly() -> UIViewController {
 		let mainViewController = MainViewController()
+		let fileProviderAdapter = FileProviderAdapter(fileProvider: FileProvider())
 
 		let mainPresenter = MainPresenter(viewController: mainViewController)
-		let mainInteractor = MainInteractor(presenter: mainPresenter)
+		let mainInteractor = MainInteractor(presenter: mainPresenter, fileProviderAdapter: fileProviderAdapter)
 		mainViewController.interactor = mainInteractor
 
 		return mainViewController
